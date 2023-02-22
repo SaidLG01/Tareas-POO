003e7f4287771e04992087f041cd3bd2ebb2fd32
Said Eduardo López García <125523838+SaidLG01@users.noreply.github.com>
2023-02-21 22:46:22 -0600

    Add files via upload

---
+++ gato.java
@@ -0,0 +1,15 @@
+public class gato{
+   public static void main(String[] args){
+	System.out.println("|-----|-----|-----|");
+	System.out.println("|     |     |     |");
+	System.out.println("|     |     |     |");
+	System.out.println("|_____|_____|_____|");
+	System.out.println("|     |     |     |");
+	System.out.println("|     |     |     |");
+	System.out.println("|_____|_____|_____|");
+	System.out.println("|     |     |     |");
+	System.out.println("|     |     |     |");
+	System.out.println("|_____|_____|_____|");
+	
+	}
+}
