fde8368dc0e2c37775626c9de614d06c5fefe009
Said Eduardo López García <125523838+SaidLG01@users.noreply.github.com>
2023-05-01 19:30:24 -0600

    Add files via upload

---
+++ Tarea 12/EventoRaton.java
@@ -0,0 +1,31 @@
+package org.example.iu.eventos;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class EventoRaton implements MouseListener {
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        System.out.println("se dió clic");
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+}
